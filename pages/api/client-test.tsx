84738a5e0711404b0b5949e0b1f2612163177645
wisysta <choi199042@gmail.com>
2022-10-10 14:00:36 +0900

    prisma 클라이언트 추가

---
+++ pages/api/client-test.tsx
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "../../libs/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.json("ok");
+}
